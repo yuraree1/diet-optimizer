7f586c17b514d3797bf2b3e63d3d85bd8cae6cb8
Yura456 <yuraree4@gmail.com>
2026-08-13 13:41:41 +0300

    Add objective function: minimize total price

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-# Main
+import pandas as pd
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum
+
+products_data=pd.read_csv("../data/products.csv")
+
+
+model=LpProblem(name="small-price", sense=LpMinimize)
+
+product_vars={x: LpVariable(name=f"{x}", lowBound=0) for x in products_data["name"]}
+
+name_price=dict(zip(products_data["name"], products_data["price"]))
+
+
+terms=[name_price[name] * product_vars[name] for name in name_price]
+
+model+=lpSum(terms)
+
+print(model)
